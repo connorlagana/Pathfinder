2ecb4b07384c3d72d5957bf4e38d877db41fb448
Connor Lagana <connor.lagana@gmail.com>
2020-08-18 16:39:52 -0400

    isfinish

--- src/components/Pathfinder.js
+++ src/components/Pathfinder.js
@@ -15,7 +15,13 @@ class Pathfinder extends Component {
     for (let row = 0; row < 95; row++) {
       const currentRow = [];
       for (let col = 0; col < 50; col++) {
-        currentRow.push([]);
+        const currentNode = {
+          col,
+          row,
+          isStart: row === 10 && col === 5,
+          isFinish: row === 10 && col === 45,
+        };
+        currentRow.push(currentNode);
       }
       nodes.push(currentRow);
     }
@@ -27,15 +33,24 @@ class Pathfinder extends Component {
     this.createGrid();
   }
   render() {
+    const { nodes } = this.state;
+    console.log(nodes);
     return (
       <div className="Pathfinder">
         <div className="grid">
-          {this.state.nodes.map((row, rowIndex) => {
+          {nodes.map((row, rowIndex) => {
             return (
-              <div>
-                {row.map((node, nodeIndex) => (
-                  <Node />
-                ))}
+              <div key={rowIndex}>
+                {row.map((node, nodeIndex) => {
+                  const { isStart, isFinish } = node;
+                  return (
+                    <Node
+                      key={nodeIndex}
+                      isStart={isStart}
+                      isFinish={isFinish}
+                    />
+                  );
+                })}
               </div>
             );
           })}
