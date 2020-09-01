8895086b6fd1bbb2ed0e3378b34f57fda6e5aa97
Connor Lagana <connor.lagana@gmail.com>
2020-09-01 13:00:15 -0400

    just needs grid

--- src/components/Pathfinder.js
+++ src/components/Pathfinder.js
@@ -12,47 +12,37 @@ class Pathfinder extends Component {
 
   createGrid = () => {
     let nodes = [];
-    for (let row = 0; row < 95; row++) {
+    for (let row = 0; row < 20; row++) {
       const currentRow = [];
       for (let col = 0; col < 50; col++) {
         const currentNode = {
           col,
           row,
-          isStart: row === 10 && col === 5,
-          isFinish: row === 10 && col === 45,
+          // isStart: row === 10 && col === 5,
+          // isFinish: row === 10 && col === 45,
         };
         currentRow.push(currentNode);
       }
       nodes.push(currentRow);
     }
+
     this.setState({
       nodes: nodes,
     });
   };
-  componentDidMount() {
-    this.createGrid();
+  async componentDidMount() {
+    await this.createGrid();
+
+    console.log(this.state);
   }
   render() {
-    const { nodes } = this.state;
-    console.log(nodes);
     return (
       <div className="Pathfinder">
         <div className="grid">
-          {nodes.map((row, rowIndex) => {
-            return (
-              <div key={rowIndex}>
-                {row.map((node, nodeIndex) => {
-                  const { isStart, isFinish } = node;
-                  return (
-                    <Node
-                      key={nodeIndex}
-                      isStart={isStart}
-                      isFinish={isFinish}
-                    />
-                  );
-                })}
-              </div>
-            );
+          {this.state.nodes.map((row, rowIndex) => {
+            return row.map((node, nodeIndex) => {
+              return <Node />;
+            });
           })}
         </div>
       </div>
